9eb92efeaca700039b1acc42de5e28a191eddacd
charensol <charensolp@gmail.com>
2021-10-01 18:14:42 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {readDir, readTextFile} from "@tauri-apps/api/fs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tp';
+export class AppComponent implements OnInit {
+  title = 'tauriTp';
+
+  treeFilesProject: any;
+
+  bool: any;
+  lastFile: any = "../";
+  currentfile: any = "../";
+
+  historicPath: Array<any> = ["../"];
+  count = 0;
+  fileValue:any='';
+
+  private TREE_DATA: any;
+
+  constructor() {
+
+  }
+
+  init() {
+    readDir("../").then(value => {
+      this.treeFilesProject = value;
+      this.TREE_DATA = value;
+      return this.TREE_DATA;
+    })
+  }
+
+  ngOnInit() {
+
+    this.TREE_DATA = this.init();
+    this.bool = 0;
+  }
+
+  click(file: any) {
+    if (this.currentfile != this.lastFile) {
+      this.lastFile = this.currentfile;
+    }
+    this.currentfile = file.path;
+
+    this.historicPath.push(file.path);
+    this.count++;
+
+    readDir(file.path).then(value => {
+      this.treeFilesProject = value;
+      console.log(value)
+      this.TREE_DATA = value;
+      return this.TREE_DATA;
+    });
+    this.bool++;
+  }
+
+  retour() {
+    readDir(this.historicPath[this.count - 1]).then(value => {
+      this.treeFilesProject = value;
+      this.TREE_DATA = value;
+      return this.TREE_DATA;
+    });
+    this.bool -= 1;
+    this.count--;
+  }
+
+  open(file:any){
+    readTextFile(file.path).then(value => {
+      this.fileValue = value;
+    })
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
